d4f200e506aa406890b4c54a5280db099ed2d354
Suyama Daichi <symdit@gmail.com>
2025-07-24 22:56:14 +0900

    feat: スプラッシュ画面の実装

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,50 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import "react-native-reanimated";
 import { TamaguiProvider } from "tamagui";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { config } from "../tamagui.config";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
 	const colorScheme = useColorScheme();
-	const [loaded] = useFonts({
+	const [appIsReady, setAppIsReady] = useState(false);
+
+	const [loaded, error] = useFonts({
 		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
 	});
 
-	if (!loaded) {
-		// Async font loading only occurs in development.
-		return null;
-	}
+	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
+	useEffect(() => {
+		if (error) throw error;
+	}, [error]);
+
+	useEffect(() => {
+		async function prepare() {
+			try {
+				// Artificially delay for 3 seconds to show the splash screen.
+				await new Promise((resolve) => setTimeout(resolve, 3000));
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				setAppIsReady(true);
+			}
+		}
+		if (loaded) {
+			prepare();
+		}
+	}, [loaded]);
+
+	useEffect(() => {
+		if (appIsReady) {
+			SplashScreen.hideAsync();
+		}
+	}, [appIsReady]);
 
 	return (
 		<TamaguiProvider config={config} defaultTheme={colorScheme ?? "light"}>
